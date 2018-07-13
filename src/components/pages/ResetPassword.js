57cac86806141c4898935e299088c47922d93e2a
Emeghara Chinedu <emeghara.augustine@gmail.com>
2018-07-13 16:59:40 +0100

    fix: Authorization issue
    fix of the issue preventing authorization
    
    BREAKING CHANGE: change to the title of some component pages

---
+++ src/components/pages/ResetPassword.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Reset_password extends Component {
+  state = {
+    password: ""
+  };
+
+  handlePasswordChange = e => {
+    this.setState({ password: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post(
+        `https://swyp-business-backend-service.herokuapp.com/api/v1/businesses/resetpassword`,
+        {
+          password: this.state.password,
+          email: "",
+          token: ""
+        }
+      )
+      .then(res => {
+        console.log(res.data.message);
+        this.setState({ message: res.data.message });
+      });
+  };
+
+  render() {
+    return (
+      <main id="main" className="container">
+        <div className="space10" />
+
+        <form className="form-signin card" onSubmit={this.handleSubmit}>
+          <div className="card-body">
+            <h1 className="h3 mb-3 font-weight-normal">
+              Please choose your new password:
+            </h1>
+            <input
+              type="text"
+              id="inputEmail"
+              className="form-control"
+              placeholder="New Password"
+              required
+              autoFocus
+              onChange={this.handlePasswordChange}
+            />
+
+            <button className="btn btn-lg btn-primary btn-block" type="submit">
+              Submit
+            </button>
+          </div>
+          <p>{this.state.message}</p>
+        </form>
+      </main>
+    );
+  }
+}
+
+export default Reset_password;

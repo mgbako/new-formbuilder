0dcd079f90b01a6a03e4eef18c5351ce91cba2c5
Mgbako John <jmgbako@gmail.com>
2018-08-03 06:20:43 +0100

    resolve the alignment

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,7 @@ import FormBuilder from "./containers/FormBuilder/FormBulder";
 import Workspaces from "./containers/Workspaces/Workspaces";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "./containers/Dashboard/Dashboard";
-import Overview from "./containers/Overview/Overview";
+//import Overview from "./containers/Overview/Overview";
 import Preview from "./containers/Preview/Preview";
 import Signup from "./containers/Signup/Signup";
 import Forms from "./containers/Forms/Forms";
@@ -33,7 +33,6 @@ export default () => (
     <PrivateRoute exact path="/workspaces" component={Workspaces} />
     <Route exact path="/resetpassword" component={ResetPassword} />
     <PrivateRoute exact path="/dashboard" component={Dashboard} />
-    <PrivateRoute exact path="/overview" component={Overview} />
     <PrivateRoute exact path="/preview" component={Preview} />
     <PrivateRoute exact path="/forms/" component={Forms} />
     <PrivateRoute exact path="/teams" component={Team} />
